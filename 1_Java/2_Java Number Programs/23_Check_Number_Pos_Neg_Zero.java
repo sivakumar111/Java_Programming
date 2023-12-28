6c4a072de179292c948227f170cf08353c241eda
KOVVURI SIVA KUMAR REDDY <140961326+sivakumar111@users.noreply.github.com>
2023-12-28 08:07:08 +0530

    23_Check_Number_Pos_Neg_Zero.java

--- 1_Java/2_Java Number Programs/23_Check_Number_Pos_Neg_Zero.java
+++ 1_Java/2_Java Number Programs/23_Check_Number_Pos_Neg_Zero.java
@@ -1,1 +1,24 @@
+// Number was Positive or Negative Number or Equels to zero 
+import java.util.*;
+public class PositiveOrNegative{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int num = sc.nextInt();
+        if(num>0){
+            System.out.println("The given number was Positive Number");
+        }else if(num<0){
+            System.out.println("The given number was Negative Number");
+        }else{
+            System.out.println("The given number equels to zero");
+        }
+        sc.close();
+    }
+}
+
+/*Output
+ *  Enter the number :
+ * 100
+ * The given number was Positive Number
+ */
 
