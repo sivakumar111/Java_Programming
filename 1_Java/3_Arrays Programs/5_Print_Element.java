19c1822c161fb416c0c5b6527885dd96cce82fe0
KOVVURI SIVA KUMAR REDDY <140961326+sivakumar111@users.noreply.github.com>
2023-12-29 21:23:26 +0530

    5_Print_Element.java

---
+++ 1_Java/3_Arrays Programs/5_Print_Element.java
@@ -0,0 +1,9 @@
+//Program to print the elements of an array
+//See the Input and Output
+import java.util.*;
+public class ArrayPrint {
+    public static void main(String[] args){
+        int[] arr = new int[] {1,2,3,4};
+        System.out.println(Arrays.toString(arr));
+    }
+}
