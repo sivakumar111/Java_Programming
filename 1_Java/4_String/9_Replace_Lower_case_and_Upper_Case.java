9ec9b60de66d788a4421f0ebe4e64c3ac175f2e3
KOVVURI SIVA KUMAR REDDY <140961326+sivakumar111@users.noreply.github.com>
2024-01-03 16:00:10 +0530

    9_Replace_Lower_case_and_Upper_Case.java

--- 1_Java/4_String/9_Replace_Lower_case_and_Upper_Case.java
+++ 1_Java/4_String/9_Replace_Lower_case_and_Upper_Case.java
@@ -1,1 +1,24 @@
+//Java Program to replace lower-case characters with upper-case and vice-versa
+import java.util.*;
+public class Change_case {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the String :");
+        String name=sc.nextLine();
+        StringBuffer newStr = new StringBuffer(name);
 
+        for(int i=0; i<name.length(); i++){
+            //Checking if the character was lower case are not
+            if(Character.isLowerCase(name.charAt(i))){
+                newStr.setCharAt(i,Character.toUpperCase(name.charAt(i)));
+            }else if(Character.isUpperCase(name.charAt(i))){
+                newStr.setCharAt(i,Character.toLowerCase(name.charAt(i)));
+            }
+        }
+        System.out.println("After the Change the Sentence :"+newStr);
+    }
+}
+/*Output:
+ * Enter the String :Siva Kumar
+ * After the Change the Sentence :sIVA kUMAR
+ */
