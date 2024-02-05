c466be44b3fb0bbef516ffef2961b1f40104628c
KOVVURI SIVA KUMAR REDDY <140961326+sivakumar111@users.noreply.github.com>
2024-02-05 21:38:26 +0530

    Cyclic.java

--- 1_Java/6_Sorting_Techniques/Cyclic_Sorting/Cyclic.java
+++ 1_Java/6_Sorting_Techniques/Cyclic_Sorting/Cyclic.java
@@ -1,1 +1,31 @@
+import java.util.Arrays;
 
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 1, 4, 2};
+        sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+
+Output:  
+Sorted array: [1, 2, 3, 4, 5, 6]
